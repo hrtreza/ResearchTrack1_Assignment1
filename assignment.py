cd17d4280d8f1f1c096a54a64c30b54d1eccb1c5
hrtreza <87908215+hrtreza@users.noreply.github.com>
2023-01-21 03:29:44 +0100

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,11 +1,17 @@
 from __future__ import print_function
 import time
 from sr.robot import *
-a_th = 2.0
+a_th = 2.0 
 d_th = 0.4
-R = Robot()
+#float: Threshold for the control of the orientation linear distance
+R = Robot() #instance of the class Robot
 
 def drive(speed, seconds):
+"""
+    Function for setting a linear velocity
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+"""
     R.motors[0].m0.power = speed
     R.motors[0].m1.power = speed
     time.sleep(seconds)
@@ -13,6 +19,12 @@ def drive(speed, seconds):
     R.motors[0].m1.power = 0
 
 def turn(speed, seconds):
+"""
+    Function for setting an angular velocity
+    
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+"""
     R.motors[0].m0.power = speed
     R.motors[0].m1.power = -speed
     time.sleep(seconds)
@@ -20,6 +32,11 @@ def turn(speed, seconds):
     R.motors[0].m1.power = 0
 
 def avoid_collision():
+	 """
+    find golden tokens and allocate the rotations to different zones
+    to detect the distance of golden tokens respect to the robot
+
+    """
   token_distances=[]
   token_rot_y=[]
   front_objects=[]
@@ -43,6 +60,12 @@ def avoid_collision():
   
 
 def find_silver_token():
+	    """
+    Generate the rotation and distance form the Silver Token respect to the robot
+    Retuens:
+    1)distance respect to the robot
+    2)rotation respect to the robot
+    """
     dist=5
     for token in R.see():
         if token.dist < dist and token.info.marker_type is MARKER_TOKEN_SILVER and -60<token.rot_y<60:
@@ -75,9 +98,9 @@ while 1:
     if silver_dist==-1 or (-a_th)*30 >= silver_rot_y >= 30*a_th:  
 	print("I don't see any token!!")
         continue
-    elif silver_dist <d_th and -30*a_th <= silver_rot_y <= 30*a_th:
+    elif silver_dist <d_th and -30*a_th <= silver_rot_y <= 30*a_th: # Condition for grabing the silver token if close enough
         print("Found it!")
-        if R.grab():
+        if R.grab():    # Uses the grab function fron Robot object
             print("Gotcha!")
 	    turn(40, 2)
 	    drive(5, 0.5)
@@ -88,10 +111,10 @@ while 1:
             print("Aww, I'm not close enough.")
     elif -a_th<= silver_rot_y <= a_th:
 	print("Ah, that'll do.")
-    elif silver_rot_y < -a_th:
-        print("Left a bit...")
+    elif silver_rot_y < -a_th:     # turn left if far from the token 
+        print("Left a bit...")   
         turn(-5, 1)
-    elif silver_rot_y > a_th:
+    elif silver_rot_y > a_th:      # turn right if far from token
         print("Right a bit...")
         turn(+5, 1)
       
